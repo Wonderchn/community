708a00c8e837ddfbdcc682562b3f74efb9dfe6e7
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-03-08 17:23:49 +0800

    牛客网首页帖子内容展示

---
+++ src/main/java/com/hongna/community/CommnityApplication.java
@@ -0,0 +1,13 @@
+package com.hongna.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CommnityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( CommnityApplication.class,args);
+    }
+
+}
---
+++ src/main/java/com/hongna/community/dao/AlphaDao.java
@@ -0,0 +1,7 @@
+package com.hongna.community.dao;
+
+public interface AlphaDao {
+
+    String select();
+
+}
